2182e91eb46b1ee32a77c618318f78c37f5d7fe0
Francis Ong <francis.ong25@gmail.com>
2022-11-17 11:53:15 +0800

    feat: add postSignUp -> create new user function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,13 @@
-exports.getSignUp = (_req, res, _next) => {
-  res.send('Hello World');
+/* Models */
+const User = require('../models');
+
+exports.postSignUp = async (req, res, _next) => {
+  try {
+    const newUser = new User(req.body);
+
+    await newUser.save();
+    res.status(201).json({ message: 'Sign-up success', user: newUser });
+  } catch (error) {
+    console.log(error);
+  }
 };
