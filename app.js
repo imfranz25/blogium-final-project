4d261893a7f9c1f519003694c2fc160064283256
Francis Ong <francis.ong25@gmail.com>
2022-11-17 21:16:58 +0800

    refactor: relocate mongoose connect from server -> app js

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-/* Core Modules */
-const path = require('path');
-
 /* 3rd Party Modules */
 const express = require('express');
+const mongoose = require('mongoose');
 
 /* File Imports */
 const useRoutes = require('./routes');
 
 /* Initialization */
 const app = express();
+require('dotenv').config();
+
+/* Database Configs */
+const MONGO_URI = process.env.MONGO_URI;
 
 /* Express Set-up -> Configs */
 app.use(express.json());
 
 /* Use all routes -> app.use(routes...) */
 useRoutes(app);
+
+/* Connect to MongoDB */
+mongoose.connect(MONGO_URI).catch((err) => {
+  console.log(`MongoDB connection error -> ${err}`);
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,23 +1,12 @@
-/* 3rd Party Module(s) */
-const mongoose = require('mongoose');
-
 /* App and Database Init */
 const app = require('./app');
 const PORT = process.env.PORT || 8080;
-const MONGO_URI = process.env.MONGO_URI;
 
 /*
  * Connect to Database then Run Server
  */
-const server = mongoose
-  .connect(MONGO_URI)
-  .then(async () => {
-    app.listen(PORT, () => {
-      console.log(`Database connected - Server running @ port ${PORT}`);
-    });
-
-    return app;
-  })
-  .catch((err) => console.log(err));
+const server = app.listen(PORT, () => {
+  console.log(`Server running @ port ${PORT}`);
+});
 
 module.exports = server;
