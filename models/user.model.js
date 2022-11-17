79f27a3f8c4099090a03b56ca55fb109b4e53ac8
Francis Ong <francis.ong25@gmail.com>
2022-11-17 21:15:02 +0800

    refactor: change custom timestamps -> mongoose schema timestamps

--- models/user.model.js
+++ models/user.model.js
@@ -1,53 +1,46 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
-  id: {
-    type: String,
-    unique: true,
-    required: true,
+const userSchema = new Schema(
+  {
+    id: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    profile_picture_url: {
+      type: String,
+    },
+    deleted_at: {
+      type: String,
+    },
+    password_chances: {
+      type: Number,
+      default: 3,
+    },
   },
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  first_name: {
-    type: String,
-    required: true,
-  },
-  last_name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  profile_picture_url: {
-    type: String,
-    required: true,
-  },
-  created_at: {
-    type: String,
-    required: true,
-  },
-  updated_at: {
-    type: String,
-    required: true,
-  },
-  deleted_at: {
-    type: String,
-    required: true,
-  },
-  password_chances: {
-    type: number,
-    default: 3,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = model('user', userSchema);
