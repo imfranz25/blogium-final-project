836613c93dee4fe55bbadbd1c89d2dc278dd8206
Francis Ong <francis.ong25@gmail.com>
2022-11-17 21:15:25 +0800

    feat: apply hash password md5

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,27 @@
-/* Models */
-const User = require('../models');
+/* 3rd Party Module(s) */
+const { validationResult } = require('express-validator');
+const { v4: uuidv4 } = require('uuid');
 
+/* Models & Helpers */
+const { User } = require('../models');
+const { hashGen } = require('../helpers');
+
+/**
+ * Create a new user
+ * @route POST /signup
+ */
 exports.postSignUp = async (req, res, _next) => {
+  const { password } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
   try {
-    const newUser = new User(req.body);
+    const id = uuidv4();
+    const hashedPassword = hashGen.generateHash(id, password);
+    const newUser = new User({ ...req.body, id: id, password: hashedPassword });
 
     await newUser.save();
     res.status(201).json({ message: 'Sign-up success', user: newUser });
