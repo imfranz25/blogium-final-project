6455734ad010da38934e0eb2ac813dfc527ec0c4
Francis Ong <francis.ong25@gmail.com>
2022-11-17 11:52:54 +0800

    refactor: update app -> for rest structure

--- app.js
+++ app.js
@@ -11,12 +11,7 @@ const useRoutes = require('./routes');
 const app = express();
 
 /* Express Set-up -> Configs */
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-/* --------------- */
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 /* Use all routes -> app.use(routes...) */
 useRoutes(app);
