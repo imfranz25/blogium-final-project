179741a027c3cac9a53c5e058d239e3a2865d22a
Francis Ong <francis.ong25@gmail.com>
2022-11-18 15:15:22 +0800

    feat: add auth middleware -> to protect blog routes

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+/**
+ * Get token from authorization header
+ * Bearer (thisIsTheToken) -> then check if it's a valid token
+ */
+exports.isAuth = (req, res, next) => {
+  const JWT_SECRET = process.env.JWT_SECRET;
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, JWT_SECRET, (error, user) => {
+      if (error) {
+        return res.status(403).json({ message: 'Invalid token' });
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json({ message: 'Log in first' });
+  }
+};
