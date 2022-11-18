9b207a9be9322e17a28d1d1606090639cdb804fc
Francis Ong <francis.ong25@gmail.com>
2022-11-18 09:04:34 +0800

    feat: implement error handlers to route index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 /* Route Imports */
 const authRoute = require('./auth.route');
 
+/* Error Handlers */
+const { errorControllers } = require('../controllers');
+let errorHandler;
+
+/* Error handlers -> Based on the environment */
+if (process.env.ENV === 'DEVELOPMENT') {
+  errorHandler = errorControllers.developmentErrors;
+} else {
+  errorHandler = errorControllers.productionErrorHandler;
+}
+
 /* Use all imported routes with app (express) */
 const useRoutes = (app) => {
   app.use(authRoute);
+  app.use('*', errorControllers.pageNotFound);
+  app.use(errorHandler);
 };
 
 module.exports = useRoutes;
