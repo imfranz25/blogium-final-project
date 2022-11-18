b99ad6b1c43371a0d5c6800a28f6f0b420b7cfcd
Francis Ong <francis.ong25@gmail.com>
2022-11-18 09:01:40 +0800

    feat: add error handler -> 404, production and development error

---
+++ controllers/error.controller.js
@@ -0,0 +1,25 @@
+/**
+ * Send 404 - Page not found -> unregistered routes
+ * @route *
+ */
+exports.pageNotFound = (_req, res, _next) => {
+  res.status(404).json({ message: 'Page not found' });
+};
+
+/**
+ * Development Error Handler
+ * Show detailed error message
+ */
+exports.developmentErrorsHandler = (error, _req, res, _next) => {
+  res.status(error.status || 500).json({ error });
+};
+
+/**
+ * Production Error Handler
+ * Omit Error Details -> send plain text "Internal Server Error"
+ */
+exports.productionErrorHandler = (error, _req, res, _next) => {
+  res.status(error.status || 500).json({
+    error: 'Internal Server Error',
+  });
+};
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 const authControllers = require('./auth.controller');
+const errorControllers = require('./error.controller');
 
 module.exports = {
   authControllers,
+  errorControllers,
 };
