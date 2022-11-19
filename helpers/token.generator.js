bc7e8b59607e93136dcef6e5cd050048f9249d87
Francis Ong <francis.ong25@gmail.com>
2022-11-19 23:10:14 +0800

    docs: add comments

--- helpers/token.generator.js
+++ helpers/token.generator.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+/**
+ * Generate token and return it with Bearer keyword -> set expiration to 1 hour
+ * @param {object} payload
+ * @returns {string}
+ */
 module.exports = (payload) => {
   const { JWT_SECRET } = process.env;
   const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
