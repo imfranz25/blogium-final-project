eeb5f18213d0b5035372d2545e372c3726588066
Francis Ong <francis.ong25@gmail.com>
2022-11-22 10:39:51 +0800

    feat: add initial components (blogs,input,navigation,profile)

---
+++ src/components/Input.js
@@ -0,0 +1,39 @@
+import { TextField, Grid, InputAdornment, IconButton } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+function Input({ half, name, handleChange, label, autoFocus, type, handleShowPassword }) {
+  let inputProps = {
+    endAdornment: (
+      <InputAdornment position="end">
+        <IconButton onClick={handleShowPassword}>
+          {type === 'password' ? <VisibilityIcon /> : <VisibilityOffIcon />}
+        </IconButton>
+      </InputAdornment>
+    ),
+  };
+
+  if (name !== 'password') {
+    inputProps = {};
+  }
+
+  return (
+    <Grid item xs={half ? 6 : 12} md={half ? 6 : 12}>
+      <TextField
+        variant="outlined"
+        size="small"
+        name={name}
+        onChange={handleChange}
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        className="input-field__auth"
+        fullWidth
+        required
+        InputProps={inputProps}
+      />
+    </Grid>
+  );
+}
+
+export default Input;
