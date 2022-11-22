819a1ff342146f8d76f96e843228f64f64f7917f
Francis Ong <francis.ong25@gmail.com>
2022-11-22 17:48:58 +0800

    feat: add file upload multer helper functions

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const passGen = require('./password.handler');
 const tokenGenerator = require('./token.generator');
+const multerUpload = require('./file-upload');
 
 module.exports = {
   passGen,
   tokenGenerator,
+  multerUpload,
 };
