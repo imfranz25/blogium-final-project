335a2145eaa4cd7037e8d5bb82b46a5aec21e884
Francis Ong <francis.ong25@gmail.com>
2022-11-23 21:05:41 +0800

    feat: add create reducer for blog

--- src/reducers/blogs.reducer.js
+++ src/reducers/blogs.reducer.js
@@ -1,9 +1,11 @@
-import { FETCH_ALL } from '../constants/actionTypes.js';
+import { FETCH_ALL, CREATE } from '../constants/actionTypes.js';
 
 const reducer = (blogs = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
       return action.payload;
+    case CREATE:
+      return blogs.push(action.payload);
     default:
       return blogs;
   }
