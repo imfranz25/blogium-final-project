87e9165c4645d604b830427ef43ff6a839367752
Francis Ong <francis.ong25@gmail.com>
2022-11-24 17:24:51 +0800

    feat: add constant for fetch -> single blog

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,5 +1,7 @@
 // Blog Type Constants
+export const FETCH = 'FETCH';
 export const FETCH_ALL = 'FETCH_ALL';
+export const FETCH_MY_BLOG = 'FETCH_MY_BLOG';
 export const CREATE = 'CREATE';
 export const DELETE = 'DELETE';
 export const UPDATE = 'UPDATE';
