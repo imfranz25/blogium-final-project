b95b105d38f47ff3a980f9e78342739d4952a241
Francis Ong <francis.ong25@gmail.com>
2022-11-24 17:22:00 +0800

    feat: add draft and get single blog fetchs

--- src/api/index.js
+++ src/api/index.js
@@ -22,10 +22,39 @@ export const fetchBlogs = (token) =>
     },
   });
 
+/* Own Blogs of the user */
+export const fetchMyBlogs = (token) =>
+  axios({
+    method: 'get',
+    url: `${API}/myblog`,
+    headers: {
+      Authorization: token,
+    },
+  });
+
+export const fetchBlog = (blogId, token) =>
+  axios({
+    method: 'get',
+    url: `${API}/${blogId}`,
+    headers: {
+      Authorization: token,
+    },
+  });
+
 export const createBlog = (blogData, token) =>
   axios({
     method: 'post',
     url: `${API}/blog/add`,
+    data: blogData,
+    headers: {
+      Authorization: token,
+    },
+  });
+
+export const draftBlog = (blogData, token) =>
+  axios({
+    method: 'post',
+    url: `${API}/blog/draft`,
     data: blogData,
     headers: {
       Authorization: token,
