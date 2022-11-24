968f0b2be642cec21909fbe0116181595ac9816a
Francis Ong <francis.ong25@gmail.com>
2022-11-24 17:24:33 +0800

    feat: add reducer for single blog

--- src/reducers/blogs.reducer.js
+++ src/reducers/blogs.reducer.js
@@ -1,8 +1,12 @@
-import { FETCH_ALL, CREATE } from '../constants/actionTypes.js';
+import { FETCH_ALL, CREATE, FETCH_MY_BLOG, FETCH } from '../constants/actionTypes.js';
 
 const reducer = (blogs = [], action) => {
   switch (action.type) {
+    case FETCH:
+      return [action.payload];
     case FETCH_ALL:
+      return action.payload;
+    case FETCH_MY_BLOG:
       return action.payload;
     case CREATE:
       return [...blogs, action.payload];
