0e8063d941ab8314ea11c2632ab4811c77db2411
Francis Ong <francis.ong25@gmail.com>
2022-11-28 10:12:03 +0800

    feat: add error to json response

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -20,6 +20,7 @@ exports.developmentErrorsHandler = (error, _req, res, _next) => {
  */
 exports.productionErrorHandler = (error, _req, res, _next) => {
   res.status(error.status || 500).json({
-    error: 'Internal Server Error',
+    error,
+    message: 'Internal Server Error',
   });
 };
