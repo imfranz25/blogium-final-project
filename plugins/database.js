174122e466f9acec302b65de4176d36467716823
Francis Ong <francis.ong25@gmail.com>
2022-12-06 10:37:39 +0800

    feat: add db add config, update root msg

---
+++ plugins/database.js
@@ -0,0 +1,11 @@
+const fp = require('fastify-plugin');
+const pgp = require('pg-promise')();
+
+const configuration = require('../config/configuration');
+
+module.exports = fp(function (fastify, opts, done) {
+  const db = pgp(configuration.databaseUri);
+  fastify.decorate('db', db);
+
+  done();
+});
