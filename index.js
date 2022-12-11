586f19b161f73462cd173c49c885b24ee2b489f7
Francis Ong <francis.ong25@gmail.com>
2022-12-11 22:01:35 +0800

    feat: add parse dashboard

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 // compatible API routes.
 require('dotenv').config();
 const express = require('express');
-const ParseServer = require('parse-server').ParseServer;
+const { ParseServer, ParseGraphQLServer } = require('parse-server');
 const path = require('path');
+const cors = require('cors');
 const args = process.argv || [];
 const test = args.some(arg => arg.includes('jasmine'));
 const { config } = require('./parse.config');
@@ -13,15 +14,44 @@ const { config } = require('./parse.config');
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
 const app = express();
+const port = process.env.PORT || 1337;
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use(express.json());
+app.use(cors());
 
-if (!test) {
-  const mountPath = process.env.PARSE_MOUNT || '/parse';
-  const api = new ParseServer(config);
+// This will enable Parse Dashboard on your dev env
+if (process.env.NODE_ENV === 'development' && !test) {
+  const ParseDashboard = require('parse-dashboard');
+  const users = [
+    {
+      user: 'admin',
+      pass: '$2y$12$zkMYmegpI00X2NmNBKHiEuujT6xl7AKWpO/Lx.3d2Gq2K2RBeulzi',
+    },
+  ];
 
-  app.use(mountPath, api);
+  app.use(
+    '/dashboard',
+    new ParseDashboard(
+      {
+        allowInsecureHTTP: true,
+        trustProxy: 1,
+        apps: [
+          {
+            serverURL: config.serverURL,
+            graphQLServerURL: config.graphQLServerURL,
+            appId: config.appId,
+            masterKey: config.masterKey,
+            appName: 'blogium-parse-server',
+          },
+        ],
+        users,
+        useEncryptedPasswords: true,
+      },
+      true
+    )
+  );
 }
 
 // Parse Server plays nicely with the rest of your web routes
@@ -36,12 +66,25 @@ app.get('/test', function (_req, res) {
 });
 
 if (!test) {
-  const port = process.env.PORT || 1337;
+  const mountPath = process.env.PARSE_MOUNT || '/parse';
+  const api = new ParseServer(config);
+
+  app.use(mountPath, api);
+
   const httpServer = require('http').createServer(app);
 
   httpServer.listen(port, function () {
     console.log('Blogium API is running on port ' + port + '.');
   });
+
+  // const parseGraphQLServer = new ParseGraphQLServer(api, {
+  //   graphQLPath: '/graphql',
+  // });
+  // parseGraphQLServer.applyGraphQL(app);
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`Parse Dashboard Running on http://localhost:${port}/dashboard.`);
+  }
 
   // This will enable the Live Query real-time server
   ParseServer.createLiveQueryServer(httpServer);
