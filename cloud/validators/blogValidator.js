4dc0c8c41c49f26816c792d7624c31f238e3add3
Francis Ong <francis.ong@stratpoint.com>
2022-12-13 22:08:57 +0800

    feat: add validator for blog creaation, update, delete

--- cloud/validators/blogValidator.js
+++ cloud/validators/blogValidator.js
@@ -1,38 +1,51 @@
 /* 3rd Party Modules */
 const validator = require('validator');
 
+/**
+ * Description
+ * @param {Number} min -> minimum characters allowed
+ * @param {String} errorMsg -> Message to show if valdiation error occured
+ * @returns {Object}
+ */
+const fieldValidator = (min, errorMsg) => {
+  return {
+    required: true,
+    type: String,
+    options: val => {
+      if (!validator.isLength(val.trim(), { min })) {
+        throw new Parse.Error(Parse.Error.VALIDATION_ERROR, errorMsg);
+      }
+
+      return true;
+    },
+  };
+};
+
 const createBlogValidator = {
   fields: {
-    title: {
+    title: fieldValidator(4, 'Blog title must be 4 characters above'),
+    description: fieldValidator(10, 'Blog description must be 4 characters above'),
+  },
+};
+
+const editBlogValidator = {
+  fields: {
+    title: fieldValidator(4, 'Blog title must be 4 characters above'),
+    description: fieldValidator(10, 'Blog description must be 4 characters above'),
+    blogId: {
       required: true,
       type: String,
-      options: val => {
-        if (!validator.isLength(val.trim(), { min: 4 })) {
-          /* prettier-ignore */
-          throw new Parse.Error(
-            Parse.Error.VALIDATION_ERROR,
-            'Title must be 4 characters above'
-          );
-        }
-
-        return true;
-      },
-    },
-    description: {
-      required: true,
-      type: String,
-      options: val => {
-        if (!validator.isLength(val.trim(), { min: 10 })) {
-          throw new Parse.Error(
-            Parse.Error.VALIDATION_ERROR,
-            'Description must be 10 characters above'
-          );
-        }
-
-        return true;
-      },
     },
   },
 };
 
-module.exports = { createBlogValidator };
+const deleteBlogValidator = {
+  fields: {
+    blogId: {
+      required: true,
+      type: String,
+    },
+  },
+};
+
+module.exports = { createBlogValidator, editBlogValidator, deleteBlogValidator };
