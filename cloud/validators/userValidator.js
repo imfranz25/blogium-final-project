9ac502a0b0f5a9ae81dcea8cec4d961f572e2ca6
Francis Ong <francis.ong@stratpoint.com>
2022-12-13 20:43:40 +0800

    refactor: add valdiator for edit and change pass

--- cloud/validators/userValidator.js
+++ cloud/validators/userValidator.js
@@ -1,68 +1,94 @@
 /* 3rd Party Modules */
 const validator = require('validator');
 
+const nameValidator = errorMsg => {
+  return {
+    required: true,
+    type: String,
+    options: val => {
+      if (!validator.isAlpha(val.trim(), 'en-US', { ignore: ' ' })) {
+        throw new Parse.Error(Parse.Error.VALIDATION_ERROR, errorMsg);
+      }
+
+      return true;
+    },
+  };
+};
+
+const emailValidator = {
+  required: true,
+  type: String,
+  options: val => {
+    if (!validator.isEmail(val.trim())) {
+      throw new Parse.Error(Parse.Error.VALIDATION_ERROR, 'Invalid email format');
+    }
+
+    return true;
+  },
+};
+
+const userNameValidator = {
+  required: true,
+  type: String,
+  options: val => {
+    if (!validator.isAlphanumeric(val.trim())) {
+      throw new Parse.Error(
+        Parse.Error.VALIDATION_ERROR,
+        'Special characters in username are not allowed'
+      );
+    }
+
+    return true;
+  },
+};
+
+const strongPasswordValidator = {
+  required: true,
+  type: String,
+  options: val => {
+    if (!validator.isStrongPassword(val.trim())) {
+      throw new Parse.Error(
+        Parse.Error.VALIDATION_ERROR,
+        'Password must be 8 characters above and have least one uppercase, lowercase, number, and a special character'
+      );
+    }
+
+    return true;
+  },
+};
+
 const createUserValidator = {
   fields: {
-    firstName: {
+    firstName: nameValidator('First name must only contain letters'),
+    lastName: nameValidator('Last name must only contain letters'),
+    email: emailValidator,
+    username: userNameValidator,
+    password: strongPasswordValidator,
+    confirmPassword: {
       required: true,
       type: String,
-      options: val => {
-        if (!validator.isAlpha(val.trim(), 'en-US', { ignore: ' ' })) {
-          throw new Parse.Error(Parse.Error.VALIDATION_ERROR, 'First name must contain letters');
-        }
-
-        return true;
-      },
     },
-    lastName: {
+  },
+};
+
+const editUserValidator = {
+  fields: {
+    firstName: nameValidator('First name must contain letters'),
+    lastName: nameValidator('Last name must contain letters'),
+    email: emailValidator,
+    username: userNameValidator,
+  },
+};
+
+const editPasswordValidator = {
+  fields: {
+    currentPassword: {
       required: true,
       type: String,
-      options: val => {
-        if (!validator.isAlpha(val.trim(), 'en-US', { ignore: ' ' })) {
-          throw new Parse.Error(Parse.Error.VALIDATION_ERROR, 'Last name must contain letters');
-        }
-
-        return true;
-      },
     },
-    email: {
+    newPassword: {
       required: true,
       type: String,
-      options: val => {
-        if (!validator.isEmail(val.trim())) {
-          throw new Parse.Error(Parse.Error.VALIDATION_ERROR, 'Invalid email format');
-        }
-
-        return true;
-      },
-    },
-    username: {
-      required: true,
-      type: String,
-      options: val => {
-        if (!validator.isAlphanumeric(val.trim())) {
-          throw new Parse.Error(
-            Parse.Error.VALIDATION_ERROR,
-            'Special characters in username are not allowed'
-          );
-        }
-
-        return true;
-      },
-    },
-    password: {
-      required: true,
-      type: String,
-      options: val => {
-        if (!validator.isStrongPassword(val.trim())) {
-          throw new Parse.Error(
-            Parse.Error.VALIDATION_ERROR,
-            'Password must be 8 characters above and have least one uppercase, lowercase, number, and a special character'
-          );
-        }
-
-        return true;
-      },
     },
     confirmPassword: {
       required: true,
@@ -71,4 +97,4 @@ const createUserValidator = {
   },
 };
 
-module.exports = { createUserValidator };
+module.exports = { createUserValidator, editUserValidator, editPasswordValidator };
