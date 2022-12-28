18e8a82ced4e4b18b6ee0dcbc0f81a5951b0f243
Francis Ong <francis.ong@stratpoint.com>
2022-12-28 10:55:58 +0800

    docs: update comments

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const args = process.argv || [];
 const test = args.some(arg => arg.includes('jasmine'));
 const { config } = require('./parse.config');
 
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
-
 const app = express();
 const port = process.env.PORT || 1337;
 
